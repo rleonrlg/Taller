86850b59381684fd5a582a7322ef824f675c533c
Marcelo Saavedra <Marcelo.Saavedra@niceincontact.com>
2022-07-26 22:04:15 -0400

    VehiculoModelo

---
+++ Schema/dbo/Tables/VehiculoModelo.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE VehiculoModelo
+(
+	CodigoModelo CHAR(3) NOT NULL ,
+	Modelo VARCHAR(64) NOT NULL ,
+	IdTaller INT NOT NULL
+);
+GO
+		ALTER TABLE VehiculoModelo
+		ADD CONSTRAINT PK_VehiculoModelo_CodigoModelo PRIMARY KEY(CodigoModelo);
+		GO
+		ALTER TABLE VehiculoModelo
+		ADD CONSTRAINT DF_VehiculoModelo_Modelo DEFAULT('') FOR Modelo;
+		GO
+		ALTER TABLE VehiculoModelo
+		ADD CONSTRAINT DF_VehiculoModelo_IdTaller DEFAULT(0) FOR IdTaller;
+		GO
+		ALTER TABLE VehiculoModelo
+		ADD CONSTRAINT CK_VehiculoModelo_CodigoModelo CHECK(LEN(CodigoModelo)=3);
+		GO
+		ALTER TABLE VehiculoModelo
+		ADD CONSTRAINT FK_VehiculoModelo_IdTaller FOREIGN KEY(IdTaller) REFERENCES Talleres(IdTaller);
